aa3818532f812d77c6f0745061c830215ba9049d
SaraFarron <jiperscreepers@gmail.com>
2022-07-14 19:40:04 +0700

    swagger 1

--- storehouse/endpoints.py
+++ storehouse/endpoints.py
@@ -77,32 +77,263 @@ class UserEndpoints(GenericEndpoints):
     model = User
     model_fields = user_fields
 
+    def get(self, model_id):
+        """
+        pass
+        """
+        return super(UserEndpoints, self).get(model_id)
+
+    def put(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(UserEndpoints, self).put(model_id)
+
+    def patch(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(UserEndpoints, self).patch(model_id)
+
+    def delete(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(UserEndpoints, self).delete(model_id)
+
 
 class VideoEndpoints(GenericEndpoints):
     model = Video
     model_fields = video_fields
 
+    def get(self, model_id):
+        """
+        pass
+        """
+        return super(VideoEndpoints, self).get(model_id)
 
-class VideosEndpoints(Resource):
+    def put(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(VideoEndpoints, self).put(model_id)
+
+    def patch(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(VideoEndpoints, self).patch(model_id)
+
+    def delete(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(VideoEndpoints, self).delete(model_id)
+
+
+class VideosEndpoints(GenericsEndpoints):
     model = Video
     model_fields = video_fields
+
+    def get(self):
+        """
+        Get all users
+        ---
+        tags:
+          - user
+        responses:
+          200:
+            description: All users data
+            schema:
+              $ref: '#/definitions/User'
+        """
+        return super(VideosEndpoints, self).get()
+
+    def post(self):
+        """
+        Create a new user
+        ---
+        tags:
+          - user
+        parameters:
+          - in: json
+            name: name
+            required: true
+            description: The username
+            type: string
+          - in: json
+            name: password
+            required: true
+            type: string
+          - in: json
+            name: email
+            required: true
+            type: string
+        """
+        return super(VideosEndpoints, self).post()
 
 
 class WatchlistEndpoints(GenericEndpoints):
     model = Watchlist
     model_fields = watchlist_fields
 
+    def get(self, model_id):
+        """
+        pass
+        """
+        return super(WatchlistEndpoints, self).get(model_id)
 
-class WatchlistsEndpoints(Resource):
+    def put(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(WatchlistEndpoints, self).put(model_id)
+
+    def patch(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(WatchlistEndpoints, self).patch(model_id)
+
+    def delete(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(WatchlistEndpoints, self).delete(model_id)
+
+
+class WatchlistsEndpoints(GenericsEndpoints):
     model = Watchlist
     model_fields = watchlist_fields
+
+    def get(self):
+        """
+        Get all users
+        ---
+        tags:
+          - user
+        responses:
+          200:
+            description: All users data
+            schema:
+              $ref: '#/definitions/User'
+        """
+        return super(WatchlistsEndpoints, self).get()
+
+    def post(self):
+        """
+        Create a new user
+        ---
+        tags:
+          - user
+        parameters:
+          - in: json
+            name: name
+            required: true
+            description: The username
+            type: string
+          - in: json
+            name: password
+            required: true
+            type: string
+          - in: json
+            name: email
+            required: true
+            type: string
+        """
+        return super(WatchlistsEndpoints, self).post()
 
 
 class FranchiseEndpoints(GenericEndpoints):
     model = Franchise
     model_fields = franchise_fields
 
+    def get(self, model_id):
+        """
+        pass
+        """
+        return super(FranchiseEndpoints, self).get(model_id)
 
-class FranchisesEndpoints(Resource):
+    def put(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(FranchiseEndpoints, self).put(model_id)
+
+    def patch(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(FranchiseEndpoints, self).patch(model_id)
+
+    def delete(self, model_id):
+        """
+
+        :param model_id:
+        :return:
+        """
+        return super(FranchiseEndpoints, self).delete(model_id)
+
+
+class FranchisesEndpoints(GenericsEndpoints):
     model = Franchise
     model_fields = franchise_fields
+
+    def get(self):
+        """
+        Get all users
+        ---
+        tags:
+          - user
+        responses:
+          200:
+            description: All users data
+            schema:
+              $ref: '#/definitions/User'
+        """
+        return super(FranchisesEndpoints, self).get()
+
+    def post(self):
+        """
+        Create a new user
+        ---
+        tags:
+          - user
+        parameters:
+          - in: json
+            name: name
+            required: true
+            description: The username
+            type: string
+          - in: json
+            name: password
+            required: true
+            type: string
+          - in: json
+            name: email
+            required: true
+            type: string
+        """
+        return super(FranchisesEndpoints, self).post()
